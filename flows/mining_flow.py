f1408a10ec92f9ef5f54877cfbce25b621b89e71
Reko-Glarius <ricardo.aliste.gonzalez@gmail.com>
2025-06-09 00:02:05 -0400

    Add system for mining and more reference image for template match

--- flows/mining_flow.py
+++ flows/mining_flow.py
@@ -1,3 +1,8 @@
+import numpy as np 
+import cv2
+import pyautogui as pg
+import pydirectinput as pdi
+
 def get_mine_starting_data():
     return "miner", "",{
         1: "primitive_iron",
@@ -8,3 +13,22 @@ def get_mine_starting_data():
         6: "",
         7: "",
     }
+
+def mining_action(screenshot_gray, button_template):
+    # Captura de pantalla actual
+    # Template match para el botón
+    res = cv2.matchTemplate(screenshot_gray, button_template, cv2.TM_CCOEFF_NORMED)
+    loc = np.where(res >= 0.7)  # Puedes ajustar el threshold si hace falta
+
+    for pt in zip(*loc[::-1]):
+        h, w = button_template.shape
+        center_x = pt[0] + w // 2
+        center_y = pt[1] + h // 2
+
+        # Mueve y hace clic izquierdo en el botón
+        pg.moveTo(center_x, center_y, duration=0.1)
+        pg.click(button='left')
+        print("Recurso recolectado.")
+        return True  # Botón encontrado y acción realizada
+
+    return False  # Botón no detectado
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # Libraries
 #################################################
 
+# Native libraries
+import time
+
 # Own Libraries
 from flows import farmer_flow, herbalist_flow
 from flows import mining_flow 
@@ -12,12 +15,14 @@ import numpy as np
 import cv2
 import pyautogui as pg
 import pydirectinput as pdi
+import keyboard
 
 #################################################
 # Variables for template Matching
 #################################################
 # Variables
-th = 0.7
+th = 0.8
+detected = None
 
 # Button for Actions
 button = cv2.imread("resources/miner/mine_icon.png")
@@ -40,18 +45,59 @@ print("Creación tempaltes terminados")
 
 print("Iniciando ciclos")
 while(True):
+    if keyboard.is_pressed('q'):
+        print("Tecla 'q' presionada. Deteniendo el script.")
+        break
+
     var = 0
 
     screenshot = cv2.cvtColor(np.array(pg.screenshot()), cv2.COLOR_RGB2BGR)
     gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
 
-    for template in resorce_templates:
-        res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
+    if(detected):
+        print("minando")
+        time.sleep(7)
+        detected = False
 
-        loc = np.where(res >= th)
+    else:
+        for template in resorce_templates:
+            res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
 
-        for i in zip(*loc[::-1]):
-            var += 1
+            loc = np.where(res >= th)
+
+            for pt in zip(*loc[::-1]):
+                h, w = template.shape  # Tamaño del template
+                center_x = pt[0] + w // 2
+                center_y = pt[1] + h // 2
+                print(center_x, center_y)
+
+                # Mueve el mouse al centro del match
+                pg.moveTo(center_x, center_y, duration=0.2)
+
+                # Clic derecho
+                pg.click(button='right')
+
+                screenshot = cv2.cvtColor(np.array(pg.screenshot()), cv2.COLOR_RGB2BGR)
+                gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+                action = mining_flow.mining_action(screenshot_gray=gray, button_template=template_gray)
+                
+                # Ejecuta la acción de minería
+                if(action):
+                    print("Action Button Detected")
+                    detected = True
+                    break
+                else:
+                    continue
+                    
+            if(detected):
+                print("Breaking for of mining points")
+                break
+        if(detected):
+                print("Breaking For of mining templates")
+                break
+    
+    time.sleep(1)
+
 
     if(var!= 0):
         pdi.keyDown('j')
